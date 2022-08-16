2a04ad208d4b73d3048b89c5af856b7fd3a888a2
Divuzki <divuzki@gmail.com>
2022-08-16 02:50:06 +0100

    Deployed :)

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn leadmanager.leadmanager.wsgi
--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'True' in os.getenv("DEBUG")
 CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = [os.getenv("SITE_URL")]
 
